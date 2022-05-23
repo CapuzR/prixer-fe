c32ef06ee3fa75fe9d9611f335af0dd21f1d0647
ggranado <ggranado@3dlinkweb.com>
2022-05-22 23:39:35 -0400

    canisterprivates

--- src/prixelart_assets/src/components/artForm.jsx
+++ src/prixelart_assets/src/components/artForm.jsx
@@ -221,6 +221,39 @@ function ArtForm({
                   },
                   blob
                 );
+                service._createPost(
+                  artist.canisterId,
+                  {
+                    artType: artType,
+                    description: aboutArt,
+                    details: [
+                      [
+                        "galleryId",
+                        { Text: galleryArt === "" ? "false" : galleryArt },
+                      ],
+                      [
+                        "location",
+                        { Text: artLocation === "" ? "false" : artLocation },
+                      ],
+
+                      [
+                        "camera",
+                        {
+                          Vec: [{ Text: artCamera }],
+                        },
+                      ],
+                      [
+                        "lens",
+                        {
+                          Vec: [{ Text: lensArt }],
+                        },
+                      ],
+                    ],
+                    tags: tagsArt,
+                    title: artTitle,
+                  },
+                  blob
+                );
                 navigate(firstArt ? "/explore" : "/main");
               }
             }
